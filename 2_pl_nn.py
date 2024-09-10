6c6ebae45e392d74cc4062f962c1e2d0f42faedd
LittleFish-Coder <adrianyu890317@gmail.com>
2024-09-10 17:27:30 +0800

    reformat nn by pl

---
+++ 2_pl_nn.py
@@ -0,0 +1,137 @@
+# imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, random_split
+from tqdm import tqdm
+import pytorch_lightning as pl
+
+# pl
+class NN(pl.LightningModule):
+    def __init__(self, input_size, num_classes):
+        super(NN, self).__init__()
+        self.fc1 = nn.Linear(in_features=input_size, out_features=50)
+        self.fc2 = nn.Linear(in_features=50, out_features=num_classes)
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+    
+    def training_step(self, batch, batch_idx):
+        loss, scores, y = self._common_step(batch, batch_idx)
+        self.log("train_loss", loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        loss, scores, y = self._common_step(batch, batch_idx)
+        self.log("val_loss", loss)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        loss, scores, y = self._common_step(batch, batch_idx)
+        self.log("test_loss", loss)
+        return loss
+
+    def _common_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.reshape(x.shape[0], -1)
+        scores = self.forward(x)
+        # loss = F.cross_entropy(scores, y)
+        loss = self.loss_fn(scores, y)
+        return loss, scores, y
+    
+    def predict_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.reshape(x.shape[0], -1)
+        scores = self.forward(x)
+        preds = torch.argmax(scores, dim=1)
+        return preds
+    
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
+        
+
+
+# set device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# hyperparameters
+input_size = 1 * 28 * 28
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 3
+
+# load data
+entire_dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = datasets.MNIST(root="dataset/", train=False, transform=transforms.ToTensor(), download=True)
+# train validation split
+train_dataset, val_dataset = random_split(entire_dataset, [50000, 10000])
+# loaders
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+# initialize
+model = NN(input_size=input_size, num_classes=num_classes).to(device)
+
+# loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# train
+for epoch in range(num_epochs):
+    for batch_idx, (data, targets) in enumerate(tqdm(train_loader)):
+        data = data.to(device)
+        targets = targets.to(device)
+
+        # flatten
+        data = data.reshape(data.shape[0], -1)
+
+        # forward
+        scores = model(data)
+        loss = criterion(scores, targets)
+
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
+
+        # gradient descent
+        optimizer.step()
+
+
+# check accuracy
+def check_accuracy(loader, model):
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for batch_idx, (data, targets) in enumerate(tqdm(loader)):
+            data = data.to(device)
+            targets = targets.to(device)
+
+            # flatten
+            data = data.reshape(data.shape[0], -1)
+
+            scores = model(data)
+
+            _, predictions = scores.max(1)
+            num_correct += (predictions == targets).sum()
+            num_samples += predictions.size(0)
+        acc = num_correct / num_samples
+        
+
+    model.train()
+    return acc
+
+
+print(f"Accuracy on training set: {check_accuracy(train_loader, model):.4f}")
+print(f"Accuracy on validation set: {check_accuracy(val_loader, model):.4f}")
+print(f"Accuracy on test set: {check_accuracy(test_loader, model):.4f}")
