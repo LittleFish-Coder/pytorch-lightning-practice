515c4c8259abd684fbab74cbf62a1856b6152393
LittleFish-Coder <adrianyu890317@gmail.com>
2024-09-10 18:26:55 +0800

    add callbacks

---
+++ 7_callbacks/train.py
@@ -0,0 +1,34 @@
+# imports
+import torch
+import pytorch_lightning as pl
+from model import NN
+from dataset import MNISTDataModule
+import config
+from callbacks import MyPrintingCallback, EarlyStopping
+
+
+if __name__ == "__main__":
+    # initialize
+    model = NN(
+        input_size=config.INPUT_SIZE,
+        learning_rate=config.LEARNING_RATE,
+        num_classes=config.NUM_CLASSES,
+    )
+    # data module
+    data_module = MNISTDataModule(
+        data_dir=config.DATA_DIR,
+        batch_size=config.BATCH_SIZE,
+        num_workers=config.NUM_WORKERS,
+    )
+    # trainer
+    trainer = pl.Trainer(
+        accelerator=config.ACCELERATOR,
+        devices=config.DEVICES,
+        min_epochs=1,
+        max_epochs=config.NUM_EPOCHS,
+        precision=config.PRECISION,
+        callbacks=[MyPrintingCallback(), EarlyStopping(monitor="val_loss")]
+    )
+    trainer.fit(model, data_module)
+    trainer.validate(model, data_module)
+    trainer.test(model, data_module)
