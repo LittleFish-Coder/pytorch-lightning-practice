df7d2c7733184dcb247833bdb30b589293860add
LittleFish-Coder <adrianyu890317@gmail.com>
2024-09-10 21:31:27 +0800

    add multi gpu

---
+++ 10_multi_gpu/model.py
@@ -0,0 +1,81 @@
+# imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import pytorch_lightning as pl
+import torchvision
+import torchmetrics
+from torchmetrics import Metric
+
+
+# pl
+class NN(pl.LightningModule):
+    def __init__(self, input_size, learning_rate, num_classes):
+        super(NN, self).__init__()
+        self.lr = learning_rate
+        self.fc1 = nn.Linear(in_features=input_size, out_features=50)
+        self.fc2 = nn.Linear(in_features=50, out_features=num_classes)
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.accuracy = torchmetrics.Accuracy(
+            task="multiclass", num_classes=num_classes
+        )
+        self.f1_score = torchmetrics.F1Score(task="multiclass", num_classes=num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        loss, scores, y = self._common_step(batch, batch_idx)
+        accuracy = self.accuracy(scores, y)
+        f1_score = self.f1_score(scores, y)
+        self.log_dict(
+            {
+                "train_loss": loss,
+                "train_accuracy": accuracy,
+                "train_f1_score": f1_score,
+            },
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+
+        if batch_idx % 100 == 0:
+            x = x[:8]
+            grid = torchvision.utils.make_grid(x.view(-1, 1, 28, 28))
+            self.logger.experiment.add_image("mnist_images", grid, global_step=self.global_step)
+
+
+        return {"loss": loss, "accuracy": accuracy, "f1_score": f1_score}
+
+    def validation_step(self, batch, batch_idx):
+        loss, scores, y = self._common_step(batch, batch_idx)
+        self.log("val_loss", loss)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        loss, scores, y = self._common_step(batch, batch_idx)
+        self.log("test_loss", loss)
+        return loss
+
+    def _common_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.reshape(x.shape[0], -1)
+        scores = self.forward(x)
+        # loss = F.cross_entropy(scores, y)
+        loss = self.loss_fn(scores, y)
+        return loss, scores, y
+
+    def predict_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.reshape(x.shape[0], -1)
+        scores = self.forward(x)
+        preds = torch.argmax(scores, dim=1)
+        return preds
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
---
+++ 10_multi_gpu/train.py
@@ -0,0 +1,50 @@
+# imports
+import torch
+import pytorch_lightning as pl
+from model import NN
+from dataset import MNISTDataModule
+import config
+from callbacks import MyPrintingCallback, EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.profilers import PyTorchProfiler
+from pytorch_lightning.strategies import DeepSpeedStrategy
+
+if __name__ == "__main__":
+    # logger
+    logger = TensorBoardLogger("tb_logs", name="mnist_model_v0")
+    profiler = PyTorchProfiler(
+        on_trace_ready=torch.profiler.tensorboard_trace_handler("tb_logs/profiler0"),
+        schedule=torch.profiler.schedule(skip_first=10, wait=1, warmup=1, active=20),
+        )
+    strategy = DeepSpeedStrategy(
+        stage=3,
+        offload_optimizer=True,
+        offload_parameters=True,
+    )
+    # initialize
+    model = NN(
+        input_size=config.INPUT_SIZE,
+        learning_rate=config.LEARNING_RATE,
+        num_classes=config.NUM_CLASSES,
+    )
+    # data module
+    data_module = MNISTDataModule(
+        data_dir=config.DATA_DIR,
+        batch_size=config.BATCH_SIZE,
+        num_workers=config.NUM_WORKERS,
+    )
+    # trainer
+    trainer = pl.Trainer(
+        strategy=strategy,
+        profiler=profiler,
+        logger=logger,
+        accelerator=config.ACCELERATOR,
+        devices=config.DEVICES,
+        min_epochs=1,
+        max_epochs=config.NUM_EPOCHS,
+        precision=config.PRECISION,
+        callbacks=[MyPrintingCallback(), EarlyStopping(monitor="val_loss")]
+    )
+    trainer.fit(model, data_module)
+    trainer.validate(model, data_module)
+    trainer.test(model, data_module)
